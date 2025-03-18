b65301a3385c967cff2448fd599e14956fe6aaa6
Naval Sonthalia <147523594+Naval4206@users.noreply.github.com>
2025-03-19 02:09:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,6 +6,6 @@ resource "aws_instance" "foo" {
   ami           = "ami-08b5b3a93ed654d19" # us-west-2
   instance_type = "t2.micro"
   tags = {
-      Name = "TF-Instance"
+      Name = "TerraForm-Instance"
   }
 }
